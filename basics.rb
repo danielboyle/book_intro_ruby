412d037f6c00018b5255c04f3149d213da6cbb5e
danielboyle <danielboyle111@gmail.com>
2015-04-23 10:02:08 -0400

    Finished Basics

---
+++ basics.rb
@@ -0,0 +1,32 @@
+# Problem 1
+"Daniel " + "Boyle"
+
+# Problem 2
+thousands = 4321 / 1000
+hundreds = 4321 % 1000 / 100
+tens = 4321 % 1000 % 100 / 10
+ones = 4321 % 1000 % 100 % 10
+
+# Problem 3
+movies = { star_wars: 1977, raiders_of_the_lost_ark: 1981, the_fugitive: 1993 }
+
+movies.each { |title, year| puts year }
+
+# Problem 4
+dates = [1977, 1981, 1993]
+
+dates.each { |year| puts year }
+
+# Problem 5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# Problem 6
+puts 1.2 ** 2
+puts 2.3 ** 2
+puts 3.4 ** 2
+
+# Problem 7
+A closing parantheses ')' was used rather than a closing curly bracket '}' which resulted in a syntax error.
